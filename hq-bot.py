206b20a7ddbec8ccacae8edc203e5ca429574094
Brett percy <brettpercy01@gmail.com>
2020-02-25 23:37:35 -0600

    updated output to colorize highest hit answer green to interpret results quicker. added some test images and the key-file for google custom search engine as well.

--- hq-bot.py
+++ hq-bot.py
@@ -12,11 +12,10 @@ from google.cloud              import vision
 from google.cloud.vision       import types
 from googleapiclient.discovery import build
 from PIL                       import Image
-#importlib.reload(sys)
-#sys.setdefaultencoding('utf8')
+
 
 # class for printing purposes
-class output:
+class Output:
    PURPLE = '\033[95m'
    CYAN = '\033[96m'
    DARKCYAN = '\033[36m'
@@ -28,18 +27,18 @@ class output:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
-class searchStats:
-    def __init__(self, count, num_results, answer, isHighestRankedAnswer):
+class SearchStats:
+    def __init__(self, count, number_of_results, answer, is_highest_ranked_answer):
         self.count = count
-        self.num_results = num_results
+        self.number_of_results = number_of_results
         self.answer = answer
-        self.isHighestRankedAnswer = isHighestRankedAnswer
+        self.is_highest_ranked_answer = is_highest_ranked_answer
 
 # sends image to google vision API to read text from
 # an image
 #
 # returns an array of text, broken by each newline (text_list)
-def googleVisionResponse(image, path):
+def google_vision_response(image, path):
     # Load credentials for using google vision
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/Brett/Development/python-sandbox/key-file.json"
 
@@ -67,7 +66,7 @@ def googleVisionResponse(image, path):
 # google, wikipedia, ask, among other sites
 #
 # returns results (res)
-def queryWeb(question):
+def query_web(question):
     service = build("customsearch", "v1", developerKey="AIzaSyBIOYJV-5hVpe8CibQoAsEDfGoz7uk_T5k")
     res = service.cse().list( q=question,  cx="009405847973959423901:pbdp0cbhche", ).execute()
     return res 
@@ -75,7 +74,7 @@ def queryWeb(question):
 
 # scrape the response from google to see how many times
 # an answer is mentioned/cited/referenced
-def scrapeQueryResults(result, answer):
+def scrape_query_results(result, answer):
     count = 0
     #print("SCRAPEQUERYRESULTS: " + answer)
     for items in result[u'items']:
@@ -90,11 +89,11 @@ def scrapeQueryResults(result, answer):
         if answer_list[i] in snippet:
             count += 1
 
-    num_results = result[u'searchInformation'][u'totalResults']
-    searchObj = searchStats(count, num_results, answer, False)
-    return searchObj
-    #percent = (int(count) * int(num_results) / 10000)
-    #print('{:4}  |  {:8}  |  {:20s}'.format(count, num_results , answer))
+    number_of_results = result[u'searchInformation'][u'totalResults']
+    search_object = SearchStats(count, number_of_results, answer, False)
+    return search_object
+    #percent = (int(count) * int(number_of_results) / 10000)
+    #print('{:4}  |  {:8}  |  {:20s}'.format(count, number_of_results , answer))
 
 
 # sets question to all lowercase and then removes
@@ -102,7 +101,7 @@ def scrapeQueryResults(result, answer):
 # operator (word boundary)
 # 
 # returns more concise question
-def makeQuestionSuccint(question):
+def make_question_succint(question):
     question = question.lower()
     question = question.replace('?', '')
     question = re.sub(r'\bwhat\b', '', question)
@@ -119,12 +118,18 @@ def makeQuestionSuccint(question):
     question = re.sub('\s\s+', ' ', question)
     return question
 
-def printResults(searchObjects):
+def print_results(search_objects_array, highest_ranked_answer):
     for i in range(1,4):
-        if searchObjects[i-1].isHighestRankedAnswer:
-            print('{:4}  |  {:8}  |  {:20s} <--'.format(searchObjects[i-1].count, searchObjects[i-1].num_results , answer_list[i]))
+        if search_objects_array[i-1].count == highest_ranked_answer:
+            print(Output.GREEN + '{:4}  |  {:8}  |  {:20s}'
+                    .format(search_objects_array[i-1].count, 
+                      search_objects_array[i-1].number_of_results,
+                        answer_list[i]) + Output.END)
         else:
-            print('{:4}  |  {:8}  |  {:20s}'.format(searchObjects[i-1].count, searchObjects[i-1].num_results , answer_list[i]))
+            print('{:4}  |  {:8}  |  {:20s}'
+                   .format(search_objects_array[i-1].count,
+                     search_objects_array[i-1].number_of_results,
+                       answer_list[i]))
 
 
 # main method, entry point of program
@@ -132,7 +137,7 @@ if __name__ == "__main__":
     img = ImageGrab.grab(bbox=(0,150,490,650)) # X1,Y1,X2,Y2
     img.save('/Users/Brett/Development/python-sandbox/testimage.png')
     
-    image_text_list = googleVisionResponse(img, 'testimage.png')
+    image_text_list = google_vision_response(img, 'testimage.png')
 
     # assign question and answer_list
     i = 0
@@ -146,13 +151,16 @@ if __name__ == "__main__":
 
     question = ' '.join(image_text_list)
 
-    question = makeQuestionSuccint(question)
+    question = make_question_succint(question)
+
     print("=====================================================================")
     print(f"Question: {question}")
     print("=====================================================================")
-    print(output.BOLD + output.UNDERLINE + 'COUNT |  NUM_RES   |  ANSWER            ' + output.END)
+    print(Output.BOLD + Output.UNDERLINE + 
+          'COUNT |  NUM_RES   |  ANSWER            '
+          + Output.END)
 
-    searchObjects = []
+    search_objects_array = []
 
     highest_ranked_answer = -1
     for i in range(1,4):
@@ -162,12 +170,12 @@ if __name__ == "__main__":
         #    questionWithAnswer = question + " " + answer_list[i]
         questionWithAnswer = question + " " + answer_list[i]
 
-        result = queryWeb(questionWithAnswer)
+        result = query_web(questionWithAnswer)
 
-        searchObjects.append(scrapeQueryResults(result, answer_list[i]))
+        search_objects_array.append(scrape_query_results(result, answer_list[i]))
         
-        # set 
-        #if searchObjects[i-1].count > highest_ranked_answer:
-            #searchObjects[i-1].isHighestRankedAnswer = True
+        # set the highest_ranked_answer to colorize the output
+        if search_objects_array[i-1].count > highest_ranked_answer:
+            highest_ranked_answer = search_objects_array[i-1].count
 
-    printResults(searchObjects)       
+    print_results(search_objects_array, highest_ranked_answer)       
